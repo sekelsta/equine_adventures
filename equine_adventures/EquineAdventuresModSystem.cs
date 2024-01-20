64298a5cb19ba492b5e5bf46b948e201a8c41e58
sekelsta <sekelsta@protonmail.com>
2024-01-20 18:25:05 -0500

    First commit

---
+++ equine_adventures/EquineAdventuresModSystem.cs
@@ -0,0 +1,26 @@
+﻿using Vintagestory.API.Client;
+using Vintagestory.API.Common;
+using Vintagestory.API.Config;
+using Vintagestory.API.Server;
+
+namespace EquineAdventures
+{
+    public class EquineAdventuresModSystem : ModSystem
+    {
+        // Called on server and client
+        public override void Start(ICoreAPI api)
+        {
+            api.Logger.Notification("Hello from equine adventures mod: " + Lang.Get("mymodid:creaturegroup-equine"));
+        }
+
+        public override void StartServerSide(ICoreServerAPI api)
+        {
+            api.Logger.Notification("Hello from equine adventures mod server side");
+        }
+
+        public override void StartClientSide(ICoreClientAPI api)
+        {
+            api.Logger.Notification("Hello from equine adventures mod client side");
+        }
+    }
+}
