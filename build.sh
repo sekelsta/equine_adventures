05a10ce5c7565ff74228698f3c31682ce83fb48c
sekelsta <sekelsta@protonmail.com>
2025-10-28 19:54:48 -0500

    Remove incomplete features and make into a content mod
    Update lang to new format

--- build.sh
+++ build.sh
@@ -2,10 +2,11 @@
 # export VINTAGE_STORY_DEV="$HOME/software/vintagestory_dev"
 
 cp assets/equineadventures/lang/es-419.json assets/equineadventures/lang/es-es.json
-dotnet run --project ./Build/CakeBuild/CakeBuild.csproj -- "$@"
+zip -r equine_adventures_indev.zip assets/ modinfo.json 
+#dotnet run --project ./Build/CakeBuild/CakeBuild.csproj -- "$@"
 rm assets/equineadventures/lang/es-es.json
-rm -r bin/
-rm -r src/obj/
-rm "${VINTAGE_STORY_DEV}"/Mods/equineadventures_*.zip
-cp Build/Releases/equineadventures_*.zip "${VINTAGE_STORY_DEV}/Mods"
+#rm -r bin/
+#rm -r src/obj/
+rm "${VINTAGE_STORY_DEV}"/Mods/equine_adventures_*.zip
+cp equine_adventures_*.zip "${VINTAGE_STORY_DEV}/Mods"
 
